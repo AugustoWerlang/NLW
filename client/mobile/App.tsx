0581ede69681144427b30b24e29672d6844abfc7
Augusto <werlang10@gmail.com>
2022-11-10 11:24:18 -0300

    conclusao aula 3

--- client/mobile/App.tsx
+++ client/mobile/App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@
 import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 import { Loading } from "./src/components/Loading";
-import { Find } from "./src/screens/Find";
+import { Pools } from "./src/screens/Pools";
 
 import { THEME } from "./src/styles/theme";
 
@@ -20,7 +20,7 @@ export default function App() {
           translucent
         />
 
-        { fontsLoaded ? <Find /> : <Loading /> }
+        { fontsLoaded ? <Pools /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
--- client/mobile/src/screens/Find.tsx
+++ client/mobile/src/screens/Find.tsx
@@ -1,6 +1,5 @@
-import { Heading, Text, VStack } from "native-base";
+import { Heading, VStack } from "native-base";
 
-import Logo from "../assets/logo.svg";
 import { Button } from "../components/Button";
 
 import { Header } from "../components/Header";
@@ -12,9 +11,7 @@ export function Find() {
       <Header title="Buscar por código" showBackButton />
 
       <VStack mt={8} mx={5} alignItems="center">
-        <Logo />
-
-        <Heading fontFamily="heading" color="white" fontSize="xl" my={8} textAlign="center">
+        <Heading fontFamily="heading" color="white" fontSize="xl" mb={8} textAlign="center">
         Encontre um bolão através de seu código único
         </Heading>
 
