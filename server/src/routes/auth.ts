d6a28f641f2317825230284e5ae8aa76b5c34d36
Augusto <werlang10@gmail.com>
2022-11-10 13:49:14 -0300

    aaa

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -11,14 +11,14 @@ export async function authRoutes(fastify: FastifyInstance) {
 
     const { access_token } = createUserBody.parse(request.body)
 
-    const userResponse = await fetch("https://www.googleapis.com/oaut2/v2/userinfo", {
+    const userResponse = await fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
       method: "GET",
       headers: {
         Authorization: `Bearer ${access_token}`,
       }
     })
 
-    const userData = await userResponse.json()
+    const userData = await userResponse.json();
 
     const userInfoSchema = z.object({
       id: z.string(),
@@ -28,7 +28,32 @@ export async function authRoutes(fastify: FastifyInstance) {
     })
 
     const userInfo = userInfoSchema.parse(userData)
-//19:56
-    return { userInfo }
+    
+    let user = await prisma.user.findUnique({
+      where: {
+        googleId: userInfo.id,
+      }
+    })
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          googleId: userInfo.id,
+          name: userInfo.name,
+          email: userInfo.email,
+          avatarUrl: userInfo.picture,
+        }
+      })
+    }
+
+    const token = fastify.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+    }, {
+      sub: user.id,
+      expiresIn: "7 days",
+    })
+
+    return { token }
   })
 }
